d9f808ea950b2d5d4f687d32076ca7d9fa961619
alexchung <yonganzhong@outlook.com>
2020-04-24 09:47:41 +0800

    remove invalid module

---
+++ VGG16_Tensorflow/test_gpu.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : test_gpu.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/4/13 下午3:17
+# @ Software   : PyCharm
+#-------------------------------------------------------
+
+import tensorflow as tf
+
+if __name__ == "__main__":
+    print(tf.test.is_gpu_available())
